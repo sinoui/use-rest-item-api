8cf806749fcb6c57f445d7c14e170ae9809736fd
刘进行 <jackingstarliu@gmail.com>
2019-12-10 13:28:35 +0800

    fix: 修复单元测试执行错误

--- src/index-extensions.spec.ts
+++ src/index-extensions.spec.ts
@@ -1,7 +1,7 @@
 import { renderHook } from '@testing-library/react-hooks';
 import useRestItemApi from '.';
 
-it.only('在id为空时，isLoading应为false', () => {
+it('在id为空时，isLoading应为false', () => {
   const { result } = renderHook(() => useRestItemApi('/users'));
 
   expect(result.current.isLoading).toBe(false);

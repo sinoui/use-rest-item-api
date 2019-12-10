972ad915277b6a1a77c4595cb616f39bc4ffdd4a
刘进行 <jackingstarliu@gmail.com>
2019-12-10 12:12:40 +0800

    fix: 修复在不指定 id 也不需要加载默认数据的情况下，isLoading 为 true 的缺陷

---
+++ src/index-extensions.spec.ts
@@ -0,0 +1,8 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useRestItemApi from '.';
+
+it.only('在id为空时，isLoading应为false', () => {
+  const { result } = renderHook(() => useRestItemApi('/users'));
+
+  expect(result.current.isLoading).toBe(false);
+});
